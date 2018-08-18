6ab5d208e51bea2532291d80290c8bbc23786c43
AravindVasudev <aravindv96@yahoo.com>
2018-08-18 17:29:20 +0530

    Added tmux.conf

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # this repo. Configure the script using the following variables:
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles
-files="bashrc bash_profile zshrc oh-my-zsh aliases vimrc virc vim" # dotfiles
+files="bashrc bash_profile zshrc oh-my-zsh aliases vimrc virc vim tmux.conf tmux.conf.local" # dotfiles
 ###############################################################################\
 
 # create olddir
