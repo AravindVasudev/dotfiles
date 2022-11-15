c2d96853fa300566a32c64a9d83ab1268e08b5c4
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2022-11-15 15:34:09 -0800

    Add checks to avoid reruns and logging

--- install.sh
+++ install.sh
@@ -9,26 +9,34 @@
 
 # Setup local configs
 cwd=$(pwd)
-backup="~/dotfiles_backup"
+backup=~/dotfiles_backup
 configs="bashrc bash_profile aliases vimrc virc"
 
-mkdir $backup
+mkdir -p $backup
 for config in $configs; do
-	if [ -e "~/.$config" ]; then
+	if [ -L "~/.$config" ] || [ -e "~/.$config" ]; then
 		echo "Moving ~/.$config to $backup."
 		mv ~/.$config $backup
 	fi
 
+    echo "symlink $cwd/$config -> ~/.$config."
 	ln -s $cwd/$config ~/.$config
 done
 
 # Install vim dependencies
 # https://github.com/junegunn/vim-plug/issues/675
+echo "Install vim plugins using vimplug."
 vim +'PlugInstall --sync' +qa
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Add aliases to .zshrc
-echo "# Common Aliases" >> ~/.zshrc
-echo "[ -e ~/.aliases ] && . ~/.aliases" >> ~/.zshrc
+if ! grep -q '. ~/.aliases' ~/.zshrc; then
+    echo "Setting up aliases for zshrc."
+    echo "# Common Aliases" >> ~/.zshrc
+    echo "[ -e ~/.aliases ] && . ~/.aliases" >> ~/.zshrc
+fi
